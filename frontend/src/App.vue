1154389dd288b22c9876c93219ca93ae01fc9396
pietaMEDS <maxibanp@gmail.com>
2024-05-19 08:15:47 +0500

    Sroki

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,18 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-      <RouterLink to="/">Home</RouterLink>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
-<style scoped>
-
-
-
-</style>
+<style scoped></style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,96 +1,142 @@
 <template>
-  <div>
-    <input type="file" @change="handleFileChange">
-    <button @click="fetchData">ЛЕСГОУ</button>
-    <div v-if="imageUrl">
-      <img :src="imageUrl" ref="image" style="max-width: 100%;">
+  <main>
+    <div>
+      <input type="file" @change="handleFileChange" />
+      <button @click="fetchData">Обработать</button>
     </div>
-    <div v-if="croppedImages.length > 0">
-      <h3>Cropped Images:</h3>
-      <div v-for="(img, index) in croppedImages" :key="index">
-        <img :src="img" :alt="'Cropped Image ' + index">
+
+    <div id="main" class="flex-around">
+      <div v-if="croppedImages.length > 0">
+        <div class="flex-around" v-for="(img, index) in croppedImages" :key="index">
+          <div style="display: flex; justify-content: space-between">
+            <select name="" id="">
+              <template v-for="(item, index) in linesType">
+                <option value="item.value">{{ item.name }}</option>
+              </template>
+              <option value="" selected>Не требуется</option>
+            </select>
+            <input type="text" />
+          </div>
+          <img :src="img" :alt="'Cropped Image ' + index" />
+        </div>
+      </div>
+
+      <div v-if="imageUrl">
+        <img :src="imageUrl" ref="image" style="max-width: 100%" />
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import axios from 'axios';
+import { ref } from 'vue'
+import axios from 'axios'
 
-const file = ref(null);
-const imageUrl = ref(null);
-const croppedImages = ref([]);
+const file = ref(null)
+const imageUrl = ref(null)
+const croppedImages = ref([])
 
 const handleFileChange = (event) => {
-  const selectedFile = event.target.files[0];
-  file.value = selectedFile;
+  const selectedFile = event.target.files[0]
+  file.value = selectedFile
 
-  const reader = new FileReader();
+  const reader = new FileReader()
   reader.onload = (e) => {
-    imageUrl.value = e.target.result;
-  };
-  reader.readAsDataURL(selectedFile);
-};
+    imageUrl.value = e.target.result
+  }
+  reader.readAsDataURL(selectedFile)
+}
+
+const linesType = [
+  {
+    value: 'CVnumber',
+    name: 'Номер трудовой книги'
+  },
+  {
+    value: 'surname',
+    name: 'Фамилия'
+  },
+  {
+    value: 'name',
+    name: 'Имя'
+  },
+  {
+    value: 'patronymic',
+    name: 'Отчество'
+  },
+  {
+    value: 'education',
+    name: 'Образование'
+  },
+  {
+    value: 'profesion',
+    name: 'Профессия, Специальность'
+  },
+  {
+    value: 'date',
+    name: 'Дата заполнения'
+  }
+]
 
 const fetchData = async () => {
-  const formData = new FormData();
-  formData.append('file', file.value);
-  formData.append('language', 'rus');
-  formData.append('isOverlayRequired', 'true');
-  formData.append('apikey', 'K83655257488957');
-  formData.append('isTable', 'true');
+  const formData = new FormData()
+  formData.append('file', file.value)
+  formData.append('language', 'rus')
+  formData.append('isOverlayRequired', 'true')
+  formData.append('apikey', 'K83655257488957')
+  formData.append('isTable', 'true')
 
   try {
-    const response = await axios.post("https://api.ocr.space/parse/image", formData, {
+    const response = await axios.post('https://api.ocr.space/parse/image', formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
       }
-    });
-
-    const data = response.data;
+    })
+    const data = response.data
+    const lines = data.ParsedResults[0].TextOverlay.Lines
     if (data.OCRExitCode === 1 && data.ParsedResults && data.ParsedResults.length > 0) {
-      const lines = data.ParsedResults[0].TextOverlay.Lines;
-      console.log(lines);
-      cropMultipleImages(lines);
+      cropMultipleImages(lines)
     } else {
-      console.error('No text found or an error occurred.');
+      console.error('No text found or an error occurred.')
     }
   } catch (error) {
-    console.error("There was an error!", error);
+    console.error('There was an error!', error)
   }
-};
+}
 
 const cropMultipleImages = (lines) => {
-  const imageElement = document.createElement('img');
-  imageElement.src = imageUrl.value;
+  const imageElement = document.createElement('img')
+  imageElement.src = imageUrl.value
 
   imageElement.onload = () => {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
 
-    croppedImages.value = [];
+    croppedImages.value = []
+    lines.forEach((line) => {
+      const left = Math.min(...line.Words.map((word) => word.Left))
+      const top = Math.min(...line.Words.map((word) => word.Top))
+      const right = Math.max(...line.Words.map((word) => word.Left + word.Width))
+      const bottom = Math.max(...line.Words.map((word) => word.Top + word.Height))
+      const width = right - left
+      const height = bottom - top
 
-    lines.forEach(line => {
-      const left = Math.min(...line.Words.map(word => word.Left));
-      const top = Math.min(...line.Words.map(word => word.Top));
-      const right = Math.max(...line.Words.map(word => word.Left + word.Width));
-      const bottom = Math.max(...line.Words.map(word => word.Top + word.Height));
-      const width = right - left;
-      const height = bottom - top;
+      canvas.width = width
+      canvas.height = height
 
-      canvas.width = width;
-      canvas.height = height;
+      ctx.drawImage(imageElement, left, top, width, height, 0, 0, width, height)
 
-      ctx.drawImage(imageElement, left, top, width, height, 0, 0, width, height);
-
-      const croppedImageUrl = canvas.toDataURL();
-      croppedImages.value.push(croppedImageUrl);
-    });
-  };
-};
+      const croppedImageUrl = canvas.toDataURL()
+      croppedImages.value.push(croppedImageUrl)
+      console.log(croppedImages)
+    })
+  }
+}
 </script>
 
 <style scoped>
-/* Добавьте любые необходимые стили */
+.flex-around {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
